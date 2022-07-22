66cede87b5e35a2ba134fb0af09011576ffb7e3f
arunkudiyal <arunkudiyal@icloud.com>
2022-07-22 14:32:29 +0530

    CSS Intro, Colors, Gradients & Box Model

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -1,5 +1,6 @@
 // Step 1 - Create object pof XHR
 const xhr = new XMLHttpRequest()
+console.log(`Ready State - ${xhr.readyState}`)
 
 // const variable = 'Ram'
 // console.log(`Hi my name is ${variable} and I am 20 years old!`)
@@ -8,19 +9,27 @@ const url = `https://api.github.com/users`
 
 // Step 2 - Open the porta; of communication b/w client & the server
 xhr.open('GET', url)
+console.log(`Ready State - ${xhr.readyState}`)
+
+// document.querySelector('#some-value').addEventListener('click', () => {
+//     console.log('CLIKCED')
+// })
+
+console.log(c)
 
 // Step 3 - Execute the function when the request state changes
 xhr.onreadystatechange  = () => {
+    console.log(`Ready State - ${xhr.readyState}`)
     if(xhr.readyState === 4 && xhr.status === 200) {
         const objectResponse = JSON.parse( xhr.responseText )
-
+        console.log(objectResponse)
         var output = ''
         for(let i=0; i < objectResponse.length; i++) {
                 output += `
                 <img src=${objectResponse[i].avatar_url} />
             `
         }
-
+        console.log(output)
         document.querySelector('#my-container').innerHTML = output
     }
 }
